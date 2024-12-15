af0c08b30651973119ad716c201424282104d1bb
NathanAlmaleh <nathanalmaleh1991@gmail.com>
2024-12-15 14:12:27 +0200

    Revert "A Hidden Gem."
    This reverts commit 68762457a99626ca16f378695a482fafe231b73c.

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,8 +1,8 @@
 module.exports = {
-    printWidth: 120,
-    tabWidth: 2,
-    semi: true,
-    singleQuote: true,
-    trailingComma: 'es5',
-    arrowParens: 'avoid',
-  };
+  printWidth: 120,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  arrowParens: 'avoid',
+};
